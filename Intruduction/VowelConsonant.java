d6fb1d579761f31fdcc32b8cdcce643b73dd054a
pattjoshi <omprakashpattjoshi123@gmail.com>
2022-06-15 18:01:09 +0530

    vowel or consonant using if..else statement

---
+++ Intruduction/VowelConsonant.java
@@ -0,0 +1,20 @@
+//  Check whether an alphabet is vowel or consonant using if..else statement
+package Intruduction;
+
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Character");
+        char ch = sc.next().charAt(0);
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+                || ch == 'O' || ch == 'U') {
+            System.out.println(ch + " is vowel");
+        } else {
+            System.out.println(ch + " is Consonant");
+        }
+        sc.close();
+    }
+}

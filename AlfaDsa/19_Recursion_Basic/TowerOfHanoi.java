1e29489f8b0b90f9f59652af56166a7814933d03
pattjoshi <omprakashpattjoshi123@gmail.com>
2022-11-25 13:41:58 +0530

    recushion

--- AlfaDsa/19_Recursion_Basic/TowerOfHanoi.java
+++ AlfaDsa/19_Recursion_Basic/TowerOfHanoi.java
@@ -1,13 +1,16 @@
 public class TowerOfHanoi {
     public static void towerOfHanoi(int n, char from_rod,
             char to_rod, char aux_rod) {
-        if (n == 0) {
+        if (n == 0) { // Base case
             return;
         }
+        // Transfer top n-1 from sec to helper using dest as 'helper'
         towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+        // Transfer nth from sec to dest
         System.out.println("Move disk " + n + " from rod "
                 + from_rod + " to rod "
                 + to_rod);
+        // Transfer n-1 from helper to dest using sec as 'helper'
         towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
     }
 

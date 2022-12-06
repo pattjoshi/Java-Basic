7ac90f410d36f59c2e385e43bddbce591f031ed8
pattjoshi <omprakashpattjoshi123@gmail.com>
2022-12-06 17:20:28 +0530

    Majority element ologn

--- AlfaDsa/20_Devide_Concore/MajorityElementOptimize.java
+++ AlfaDsa/20_Devide_Concore/MajorityElementOptimize.java
@@ -42,7 +42,7 @@ public class MajorityElementOptimize {
     }
 
     public static void main(String[] args) {
-        int nums[] = { 2, 1, 1, 1, 2, 1, 1, 1, 2, 2 };
+        int nums[] = { 2, 1, 1, 2, 2 };
         System.out.println(majorityElement(nums));
 
     }
